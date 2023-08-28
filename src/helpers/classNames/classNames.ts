0c9aeb2abb61dc3a36ef4d4e333846456c4be55c
Merdzhen <merdzhen.k@gmail.com>
2023-08-28 10:11:38 +0300

    initial commit

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,14 @@
+type Mods = Record<string, boolean | string>
+
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
+  return [
+    cls,
+    Object.entries(mods)
+      .filter(([className, value]) => Boolean(value))
+      .map(([className, value]) => className),
+    ...additional,
+  ].join(' ')
+}
+
+// пример - вызов classNames('remove-btn', {hovered: true, selectable: true, red: false}, ['pdg'])
+// результат 'remove-btn hovered selectable pdg'
